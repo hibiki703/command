1c3d9ca31d2a77b82fc417ad1f7426c3107eca1c
hibiki703 <hibiki703@LAPTOP-5J6C32VG.localdomain>
2024-01-14 13:30:49 +0900

    update test.bash

--- test.bash
+++ test.bash
@@ -11,8 +11,17 @@ ng(){
 res=0
 
 ### I/O TEST ###
-out=e
-[ "${out}" = "e" ] || ng "$LINENO"
+out=$(echo "Hello! 123" | ./count)
+[ "${out}" = '
+文字列を入力 :
+入力された文字数: 10
+大文字の数      : 1
+小文字の数      : 4
+数字の数        : 3
+空白の数        : 1
+その他          : 1' ] || ng "$LINENO"
 
 ### STRANGE INPUT ###
+
+
 exit $res
